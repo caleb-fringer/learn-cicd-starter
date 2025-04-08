8a82a4516481e7c0b80dce929eea4db8da40d702
Caleb Fringer <cfringer98@gmail.com>
2025-04-07 21:54:16 -0700

    Add (broken) unit tests in CI

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKeyBlankHeader(t *testing.T) {
+	t.Fatalf("FAILING TEST TO TEST CI")
+	header := http.Header{}
+	_, err := GetAPIKey(header)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("Should be ErrNoAuthHeaderIncluded, got %v", err)
+	}
+}
+
+func TestGetAPIKeyMalformedHeader(t *testing.T) {
+	malformedHeader := http.Header{}
+	malformedHeader.Add("Authorization", "ApiKey")
+
+	_, err := GetAPIKey(malformedHeader)
+	if err == nil {
+		t.Fatalf("Expected malformed authorization header")
+	}
+}
+
+func TestGetAPIKeyGood(t *testing.T) {
+	header := http.Header{}
+	header.Add("Authorization", "ApiKey supersecurekey")
+
+	key, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if key != "supersecurekey" {
+		t.Fatalf("Expected value 'supersecurekey', got %v", key)
+	}
+}
